8a4a86221020ffc604fa731ae7b6de418a62b771
David McGrann <dpmcgrann@gmail.com>
2019-08-01 17:02:30 -0400

    update to test signup form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import LoginForm from './components/LoginForm.js';
+// import LoginForm from './components/LoginForm.js';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/users.js'
-// import SignupForm from './components/SignupForm.js'
+import SignupForm from './components/SignupForm.js'
 
 class App extends React.Component {
 
@@ -14,7 +14,8 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <LoginForm/>
+    
+        <SignupForm/>
       </div>
     );
   }
