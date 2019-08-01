297a5d6b25b7905e5595ab1410e2c3954c8b4d78
David McGrann <dpmcgrann@gmail.com>
2019-08-01 12:06:25 -0400

    add current user action

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -1,3 +1,10 @@
 // post request to signup named userSignup
 // post request to login named userLogin
 // get reguest to find current named getCurrentUser
+
+export const setCurrentUser = user => {
+  return {
+    type: "SET_CURRENT_USER",
+    user
+  }
+}
