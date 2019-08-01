1abd5af321f169be470d28d08b8663c49f6a3a59
David McGrann <dpmcgrann@gmail.com>
2019-08-01 13:01:55 -0400

    work on refactor of login form to turn it into a functional component

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,52 +1,94 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {userLogin} from '../actions/users';
+import {updateLogin} from '../actions/login';
 
-class LoginForm extends Component {
-  state = {
-    email: "",
-    password: ""
-  }
+const LoginForm = ({ email, password, updateLogin }) => {
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
+  const handleChange = event => {}
 
-  handleSubmit = event => {
-    event.preventDefault()
-    this.props.userLogin(this.state)
-  }
+  const handleSubmit = event => {}
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h1>Login</h1>
 
-        <label>Email</label>
-        <input
-          name='email'
-          value={this.state.email}
-          onChange={this.handleChange}
-          /><br/>
+  return (
+    <form onSubmit={this.handleSubmit}>
+            <h1>Login</h1>
 
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          value={this.state.password}
-          onChange={this.handleChange}
-          /><br/>
+            <label>Email</label>
+            <input
+              name='email'
+              value={email}
+              onChange={this.handleChange}
+              /><br/>
 
-        <input type='submit'/>
-      </form>
-    )
+            <label>Password</label>
+            <input
+              type='password'
+              name='password'
+              value={password}
+              onChange={this.handleChange}
+              /><br/>
+
+            <input type='submit'/>
+          </form>
+
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    email: state.login.email,
+    password: state.login.password
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  userLoginFetch: userInfo => dispatch(userLogin(userInfo))
-})
+export default connect(mapStateToProps, { updateLogin })(LoginForm);
 
-export default connect(null, mapDispatchToProps)(LoginForm);
+// class LoginForm extends Component {
+//   state = {
+//     email: "",
+//     password: ""
+//   }
+//
+//   handleChange = event => {
+//     this.setState({
+//       [event.target.name]: event.target.value
+//     });
+//   }
+//
+//   handleSubmit = event => {
+//     event.preventDefault()
+//     this.props.userLogin(this.state)
+//   }
+//
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <h1>Login</h1>
+//
+//         <label>Email</label>
+//         <input
+//           name='email'
+//           value={this.state.email}
+//           onChange={this.handleChange}
+//           /><br/>
+//
+//         <label>Password</label>
+//         <input
+//           type='password'
+//           name='password'
+//           value={this.state.password}
+//           onChange={this.handleChange}
+//           /><br/>
+//
+//         <input type='submit'/>
+//       </form>
+//     )
+//   }
+// }
+//
+// const mapDispatchToProps = dispatch => ({
+//   userLoginFetch: userInfo => dispatch(userLogin(userInfo))
+// })
+//
+// export default connect(null, mapDispatchToProps)(LoginForm);
