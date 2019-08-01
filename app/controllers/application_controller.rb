fc01f8f494b56ee18c174b8905b449b5f091fa8f
David McGrann <dpmcgrann@gmail.com>
2019-08-01 14:46:57 -0400

    include cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-
+   include ::ActionController::Cookies
+   
   def current_user
     User.find(session[:user_id])
   end
@@ -7,5 +8,5 @@ class ApplicationController < ActionController::API
   def logged_in?
     !!current_user
   end
-  
+
 end
