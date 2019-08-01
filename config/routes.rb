ae3e33004a1e1ea65e9f7e30590f9bf409c1b334
David McGrann <dpmcgrann@gmail.com>
2019-08-01 14:18:08 -0400

    fix typos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  post '/api/vi/login', to: 'api/vi/sessions#create'
+  post '/api/v1/login', to: 'api/v1/sessions#create'
   get "/api/v1/current_user", to: "api/v1/sessions#get_user"
 
   namespace :api do
