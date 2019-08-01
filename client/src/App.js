eb991f3e2b17ca16b5e4c2b4524655c7bda9352e
David McGrann <dpmcgrann@gmail.com>
2019-08-01 11:44:36 -0400

    add components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import LoginForm from './component/LoginForm'
+import SignupForm from './component/SignupForm'
 
 class App extends React.Component {
 
