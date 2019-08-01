6716a6b054b2736702bca631fa8fdf78e56c7ff3
David McGrann <dpmcgrann@gmail.com>
2019-08-01 11:45:03 -0400

    add reducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 // reducers will go here
-
+import users from '../reducers/users'
 import thunk from 'redux-thunk'
 
 
 const reducer = combineReducers({
   // reducers here
+  users
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
