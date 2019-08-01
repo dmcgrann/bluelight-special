70fadc168cc5316d6e59d8ae075109dce69a420a
David McGrann <dpmcgrann@gmail.com>
2019-08-01 17:00:01 -0400

    update create to create a session and update show to use serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,13 +10,15 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user
+    user_json = UserSerializer.new(@user).serialized_json
+    render json: user_json
   end
 
   # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       render json:UserSerializer.new(@user), status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -45,6 +47,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
+      params.require(:user).permit(:name, :email, :password)
     end
 end
