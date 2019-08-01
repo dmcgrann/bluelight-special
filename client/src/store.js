c03396aa21aa7ab2ade498e5c2a25eafd915c27a
David McGrann <dpmcgrann@gmail.com>
2019-08-01 17:01:18 -0400

    add signup reducer

--- client/src/store.js
+++ client/src/store.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 // reducers will go here
 import users from './reducers/users'
 import login from './reducers/login'
+import signup from './reducers/signup'
 import thunk from 'redux-thunk'
 
 
 const reducer = combineReducers({
   // reducers here
   users,
-  login
+  login,
+  signup
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
