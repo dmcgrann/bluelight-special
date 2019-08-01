ca4e4ba923454e6effee861abee9022f846780b0
David McGrann <dpmcgrann@gmail.com>
2019-08-01 14:17:45 -0400

    update create user function

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,16 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :ok
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
   end
 
   def get_user
