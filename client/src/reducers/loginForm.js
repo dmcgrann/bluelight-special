32be4cf60b71cf1a8a8d3f3b28cd3d6eb354dd97
David McGrann <dpmcgrann@gmail.com>
2019-08-02 10:49:41 -0400

    rename reducer files

--- client/src/reducers/loginForm.js
+++ client/src/reducers/loginForm.js
@@ -1,13 +1,14 @@
 const initialState = {
-  name: "",
   email: "",
   password: ""
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case "UPDATE_SIGNUP_FORM":
+    case "UPDATE_FORM":
       return action.userInfo
+    case "CLEAR_FORM":
+      return initialState
     default:
       return state
   }
--- client/src/reducers/signupForm.js
+++ client/src/reducers/signupForm.js
@@ -1,12 +1,15 @@
 const initialState = {
+  name: "",
   email: "",
   password: ""
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case "UPDATE_LOGIN_FORM":
+    case "UPDATE_FORM":
       return action.userInfo
+    case "CLEAR_FORM":
+      return initialState
     default:
       return state
   }
