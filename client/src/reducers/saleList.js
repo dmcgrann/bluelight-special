2efcd880f71685fced53d4f484e8a67f00dab28e
David McGrann <dpmcgrann@gmail.com>
2019-08-03 13:32:31 -0400

    update reducer

--- client/src/reducers/saleList.js
+++ client/src/reducers/saleList.js
@@ -1,10 +1,7 @@
-const initialState = []
-
-export default (state = initialState, action) => {
+export default (state = null, action) => {
   switch (action.type) {
-
-    case 'SET_SALES_LIST':
-        return action.sales
+    case "SET_SALE_LIST":
+      return action.sales
 
     default:
       return state;
