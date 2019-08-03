fdc05f71a88399e72a8853e3da2aa2030969e0f7
David McGrann <dpmcgrann@gmail.com>
2019-08-03 13:31:45 -0400

    remove associations from serializers

--- app/serializers/sale_serializer.rb
+++ app/serializers/sale_serializer.rb
@@ -1,5 +1,4 @@
 class SaleSerializer
   include FastJsonapi::ObjectSerializer
   attributes :address, :days, :notes
-  belongs_to :user, serializer: UserSerializer
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :email
-  has_one :sale, serializer: SaleSerialzer
 end
