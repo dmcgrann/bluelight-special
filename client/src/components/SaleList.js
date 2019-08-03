946165f49db4990a11f77c4e0ee8abfe97f9435f
David McGrann <dpmcgrann@gmail.com>
2019-08-03 17:16:14 -0400

    add condition to SaleList and add BrowserRouter to index

--- client/src/components/SaleList.js
+++ client/src/components/SaleList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { getSales } from '../actions/saleList'
 
 class SaleList extends React.Component {
 
    render() {
      return(
-
-       this.props.saleList.map(sale => (<p key={sale.id}>{sale.attributes.address}</p>))
-
-
+       this.props.saleList != null ?
+       this.props.saleList.map(sale => (<p key={sale.id}>{sale.attributes.address}</p>)) :
+       "Please login"
      )
    }
 
