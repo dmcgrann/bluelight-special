56a9ef814746a742598b0e4627feaaa4445fc209
David McGrann <dpmcgrann@gmail.com>
2019-08-03 19:21:20 -0400

    add link and list html to sale list

--- client/src/components/SaleList.js
+++ client/src/components/SaleList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const SaleList = props => {
   const currentSales = props.sales != null ?
-  props.sales.map(sale => (<p key={sale.id}>{sale.attributes.address}</p>)) :
+  props.sales.map(sale => (<ul><li key={sale.id}><Link to={`/sales/${sale.id}`}>{sale.attributes.address}</Link></li></ul>)) :
   <p>"There are not any current sales."</p>
 
 
