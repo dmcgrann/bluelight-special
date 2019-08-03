06ffabe03200e1283aa3709ccb2874177e16c1df
David McGrann <dpmcgrann@gmail.com>
2019-08-03 16:42:52 -0400

    update to hadle routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
 import { getCurrentUser, logout} from './actions/users'
 import {getSales} from './actions/saleList'
 import SignupForm from './components/SignupForm'
 import LoginForm from './components/LoginForm';
 import Logout from './components/Logout';
 import SaleList from './components/SaleList'
+import NavBar from './components/NavBar'
 
 class App extends React.Component {
 
@@ -17,10 +19,16 @@ class App extends React.Component {
 
   render(){
     return (
-      <div>
-        <SaleList />
-        <LoginForm/>
-        </div>
+      <Router>
+      <div className="app">
+        <NavBar />
+        <Route exact path="/" />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/logout" component={Logout} />
+        <Route exact path="/signup" component={SignupForm} />
+        <Route exact path="/sales" component={SaleList} />
+      </div>
+    </Router>
     );
   }
 }
