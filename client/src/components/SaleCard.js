ffa57f763acb6a8bec6747e584eb54264910905c
David McGrann <dpmcgrann@gmail.com>
2019-08-04 16:53:06 -0400

    create component to display a sale

---
+++ client/src/components/SaleCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SaleCard = ({sale}) => {
+  return (
+    <div>
+    <p>{user.attributes.address}</p>
+    <p>{user.attributes.days}</p>
+    <p>{user.attributes.notes}</p>
+  )
+}
+
+export default UserCard
