355282f8fe554d9ae53c230c475b52981f58b6af
David McGrann <dpmcgrann@gmail.com>
2019-08-04 14:04:07 -0400

    fix update sale action

--- client/src/actions/sales.js
+++ client/src/actions/sales.js
@@ -1,4 +1,5 @@
-export const updateSale = saleInfo => {
+export const updateSale = (name, value) => {
+  const saleInfo = {name,value}
   return {
     type: "UPDATE_SALE",
     saleInfo
@@ -8,5 +9,41 @@ export const updateSale = saleInfo => {
 export const clearSale = () => {
   return {
     type: "CLEAR_SALE"
+  }
+}
+
+const setCurrentSale = sale => {
+  return {
+    type: "SET_CURRENT_SALE",
+    sale
+  }
+}
+
+export const createSale = (details, history) => {
+  return dispatch => {
+    const saleData = {
+      address: details.address,
+      days: details.days,
+      notes: details.notes,
+      user_id: details.userId
+    }
+    return fetch("http://localhost:3001/api/v1/sales", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(saleData)
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch(setCurrentSale(response.data))
+          history.push('/')
+        }
+      })
+      .catch(console.log)
   }
 }
