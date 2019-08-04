8150a47fb435188352022a6b44a451582468dde4
David McGrann <dpmcgrann@gmail.com>
2019-08-03 23:12:47 -0400

    add footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux'
+import Logout from './Logout'
+
+const Footer = ( {currentUser, loggedIn} ) => {
+  return (
+    <div className="footer">
+
+      { loggedIn ?
+        <>
+        <p>Logged in as {currentUser.attributes.email}.</p>
+        <p>Copyright (c) 2019. All Rights Reserved.</p>
+        <Logout />
+        </>
+         :
+        null
+          }
+    </div>
+  );
+};
+
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser,
+    loggedIn: !!currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Footer);
