a6edf158d7489b1ad1cd89222b44d82261661527
David McGrann <dpmcgrann@gmail.com>
2019-08-04 19:26:38 -0400

    remove unused logout component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux'
-import Logout from './Logout'
 
 const NavBar = ( {currentUser, loggedIn} ) => {
   return (
