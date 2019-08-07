67380a936843151260bccb0880b650756c0f5927
David McGrann <dpmcgrann@gmail.com>
2019-08-07 19:37:09 -0400

    add link to edit route

--- client/src/components/EditSaleCard.js
+++ client/src/components/EditSaleCard.js
@@ -1,19 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-
-
-const EditSale = ({sale}) => {
+const EditSaleCard = ({sale}) => {
   return(
     <div>
     <p>{sale.attributes.address}</p>
     <p>{sale.attributes.days}</p>
     <p>{sale.attributes.notes}</p>
-    
-    <>
-    <button>Edit Sale</button>
-    </>
+    <p><Link to={`/sales/${sale.id}/edit`}>Edit Your Sale</Link></p>
     </div>
   )
 }
 
-export default EditSale
+export default EditSaleCard
