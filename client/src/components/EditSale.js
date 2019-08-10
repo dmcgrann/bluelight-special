0c012e31a6063f1f728890ecde781ae09f591047
David McGrann <dpmcgrann@gmail.com>
2019-08-09 21:06:12 -0400

    set handlechange function

--- client/src/components/EditSale.js
+++ client/src/components/EditSale.js
@@ -1,42 +1,79 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import {createSale} from '../actions/sales'
+import {updateSaleForm} from '../actions/saleForm';
 
 class EditSale extends React.Component {
   constructor(props){
     super(props)
     this.state = {
       isEditing: false,
-      sale: this.props.sale
+      sale: this.props.sale.attributes
     };
     this.toggleEdit = this.toggleEdit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   toggleEdit() {
     this.setState({isEditing: !this.state.isEditing})
   }
 
+  handleChange(event) {
+    const field = event.target.name;
+    const sale = this.state.sale;
+    sale[field] = event.target.value;
+    return this.setState({sale: sale});
+    };
 
+  handleSubmit() {
+
+  }
 
   render(){
     if (this.state.isEditing) {
       return (
         <div>
-        <p>form</p>
+        <form >
+          <h1>Update Your Garagesale</h1>
+
+          <label>Address</label>
+          <input
+            name='address'
+            value={this.state.sale.address}
+            onChange={this.handleChange}
+            /><br/>
+
+          <label>Days</label>
+          <input
+            name='days'
+            value={this.state.sale.days}
+            onChange={this.handleChange}
+            /><br/>
+
+          <label>Notes</label>
+          <input
+            name='notes'
+            value={this.state.sale.notes}
+            onChange={this.handleChange}
+            /><br/>
+
+          <input type='submit'/>
+        </form>
+      )
         </div>
       )
     }
     return(
       <div>
-      <p>{this.state.sale.attributes.address}</p>
-      <p>{this.state.sale.attributes.days}</p>
-      <p>{this.state.sale.attributes.notes}</p>
+      <p>{this.state.sale.address}</p>
+      <p>{this.state.sale.days}</p>
+      <p>{this.state.sale.notes}</p>
 
       <button onClick={this.toggleEdit}>edit</button>
       </div>
     )
   }
 
-
 }
 
-export default EditSale
+export default connect(null, { createSale })(EditSale);
