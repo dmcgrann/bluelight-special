bfe50b78b85ef59666e7af251d9967249832eba3
David McGrann <dpmcgrann@gmail.com>
2019-08-20 12:29:31 -0400

    move form inputs to new folder

--- client/src/components/formFields/EmailInput.js
+++ client/src/components/formFields/EmailInput.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const PasswordInput = props => {
+const EmailInput = props => {
 
     let formControl = "form-control";
 
-    if (props.touched && !props.valid) {
+    if (!props.valid) {
         formControl = 'form-control control-error';
     }
 
     return (
         <div className="form-group">
-            <input type="password" className={formControl} {...props} />
+            <input type="email" className={formControl} {...props} />
         </div>
     );
 }
 
-export default PasswordInput;
+export default EmailInput;
--- client/src/components/formFields/PasswordInput.js
+++ client/src/components/formFields/PasswordInput.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const EmailInput = props => {
+const PasswordInput = props => {
 
     let formControl = "form-control";
 
-    if (props.touched && !props.valid) {
+    if (!props.valid) {
         formControl = 'form-control control-error';
     }
 
     return (
         <div className="form-group">
-            <input type="email" className={formControl} {...props} />
+            <input type="password" className={formControl} {...props} />
         </div>
     );
 }
 
-export default EmailInput;
+export default PasswordInput;
