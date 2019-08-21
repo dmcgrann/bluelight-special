beb23d1369c2f515c55030c44db7e7fcbca68b95
David McGrann <dpmcgrann@gmail.com>
2019-08-20 21:31:16 -0400

    create home component

---
+++ client/src/components/common/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import background from '../../background.jpg'
+import style from './style.css'
+
+const Home = () => {
+  return <img src={background} alt="Logo" />;
+}
+
+export default Home
