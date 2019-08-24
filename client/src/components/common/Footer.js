5b35f2a5dc504609716143543766ffb67f54466a
David McGrann <dpmcgrann@gmail.com>
2019-08-24 11:13:29 -0400

    move user message to navbar

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux'
 
 const Footer = ( {currentUser, loggedIn} ) => {
   return (
     <div className="footer">
 
-      { loggedIn ?
-        <>
-        <p id="footertext">Logged in as {currentUser.attributes.email}.</p>
-        <p>Copyright (c) 2019. All Rights Reserved.</p>
-
-        </>
-         :
         <p id="footertext">Copyright (c) 2019. All Rights Reserved.</p>
-          }
+
     </div>
   );
 };
 
-const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser,
-    loggedIn: !!currentUser
-  }
-}
 
-export default connect(mapStateToProps)(Footer);
+export default Footer
