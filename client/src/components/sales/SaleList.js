386b410a2e96da2dae0db970b64bd53144dbedfa
David McGrann <dpmcgrann@gmail.com>
2019-08-30 19:02:14 -0400

    add button. log clicks successfully.

--- client/src/components/sales/SaleList.js
+++ client/src/components/sales/SaleList.js
@@ -4,26 +4,45 @@ import { Link } from 'react-router-dom'
 import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
 import banner1 from '../../banner1.png'
 
-const SaleList = props => {
-  const currentSales = props.sales != null ?
-  props.sales.map(sale => (<ListGroup><ListGroupItem key={sale.id}><Link to={`/sales/${sale.id}`}>{sale.attributes.address}</Link></ListGroupItem></ListGroup>)) :
-  <p>"There are not any current sales."</p>
+
+class SaleList extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      votes: []
+
+    }
+  }
+
+  upVote = () => {
+
+    console.log("i was clicked")
+
+  }
 
 
-   return(
-     <Card>
-       <Card.Img variant="top" src={banner1} />
-       <Card.Body>
-         <ListGroup className="list-group-flush">
-           <h1>Current Listings</h1>
-           <ListGroupItem>
-             {currentSales}
-           </ListGroupItem>
-         </ListGroup>
-        </Card.Body>
-     </Card>
 
-   )
+  render() {
+    const currentSales = this.props.sales != null ?
+    this.props.sales.map(sale => (<ListGroup><ListGroupItem key={sale.id}><Link to={`/sales/${sale.id}`}>{sale.attributes.address}</Link> <button onClick={this.upVote}>Vote</button> </ListGroupItem></ListGroup>)) :
+    <p>"There are not any current sales."</p>
+
+
+     return(
+       <Card>
+         <Card.Img variant="top" src={banner1} />
+         <Card.Body>
+           <ListGroup className="list-group-flush">
+             <h1>Current Listings</h1>
+             <ListGroupItem>
+               {currentSales}
+             </ListGroupItem>
+           </ListGroup>
+          </Card.Body>
+       </Card>
+
+     )
+  }
 }
 
 const mapStateToProps = state => {
