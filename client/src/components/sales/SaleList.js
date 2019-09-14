d3b38967d8dc2b9fea00273ef6e76b17db67207a
David McGrann <dpmcgrann@gmail.com>
2019-09-14 08:55:46 -0400

    return salelist to original

--- client/src/components/sales/SaleList.js
+++ client/src/components/sales/SaleList.js
@@ -4,48 +4,26 @@ import { Link } from 'react-router-dom'
 import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
 import banner1 from '../../banner1.png'
 
-
-class SaleList extends React.Component {
-  constructor(props) {
-    super()
-    this.state = {
-      votes: 0
-
-    }
-  }
-
-  upVote = () => {
-
-    console.log("i was clicked")
-    this.setState((prevState, {votes}) => ({
-      votes: prevState.votes + 1
-    }))
-
-  }
+const SaleList = props => {
+  const currentSales = props.sales != null ?
+  props.sales.map(sale => (<ListGroup><ListGroupItem key={sale.id}><Link to={`/sales/${sale.id}`}>{sale.attributes.address}</Link></ListGroupItem></ListGroup>)) :
+  <p>"There are not any current sales."</p>
 
 
+   return(
+     <Card>
+       <Card.Img variant="top" src={banner1} />
+       <Card.Body>
+         <ListGroup className="list-group-flush">
+           <h1>Current Listings</h1>
+           <ListGroupItem>
+             {currentSales}
+           </ListGroupItem>
+         </ListGroup>
+        </Card.Body>
+     </Card>
 
-  render() {
-    const currentSales = this.props.sales != null ?
-    this.props.sales.map(sale => (<ListGroup><ListGroupItem key={sale.id}><Link to={`/sales/${sale.id}`}>{sale.attributes.address}</Link> <button onClick={this.upVote}>Vote</button><span>{this.state.votes}</span></ListGroupItem></ListGroup>)) :
-    <p>"There are not any current sales."</p>
-
-
-     return(
-       <Card>
-         <Card.Img variant="top" src={banner1} />
-         <Card.Body>
-           <ListGroup className="list-group-flush">
-             <h1>Current Listings</h1>
-             <ListGroupItem>
-               {currentSales}
-             </ListGroupItem>
-           </ListGroup>
-          </Card.Body>
-       </Card>
-
-     )
-  }
+   )
 }
 
 const mapStateToProps = state => {
@@ -54,4 +32,5 @@ const mapStateToProps = state => {
   }
 }
 
-  export default connect(mapStateToProps)(SaleList)
+
+export default connect(mapStateToProps)(SaleList)
