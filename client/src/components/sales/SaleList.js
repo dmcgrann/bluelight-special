bf4ece9e6882be1b236c826a2e2fb55c5c682291
David McGrann <dpmcgrann@gmail.com>
2019-09-14 09:13:28 -0400

    switch back to class to use react search

--- client/src/components/sales/SaleList.js
+++ client/src/components/sales/SaleList.js
@@ -4,26 +4,52 @@ import { Link } from 'react-router-dom'
 import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
 import banner1 from '../../banner1.png'
 
-const SaleList = props => {
-  const currentSales = props.sales != null ?
-  props.sales.map(sale => (<ListGroup><ListGroupItem key={sale.id}><Link to={`/sales/${sale.id}`}>{sale.attributes.address}</Link></ListGroupItem></ListGroup>)) :
-  <p>"There are not any current sales."</p>
+
+class SaleList extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      search: ''
+    }
+  }
+
+  handleSearch = event => {
+    this.setState({ search: event.target.value.toLowerCase()
+    })
+  }
 
 
-   return(
-     <Card>
-       <Card.Img variant="top" src={banner1} />
-       <Card.Body>
-         <ListGroup className="list-group-flush">
-           <h1>Current Listings</h1>
-           <ListGroupItem>
-             {currentSales}
-           </ListGroupItem>
-         </ListGroup>
-        </Card.Body>
-     </Card>
 
-   )
+
+  render() {
+
+    const saleSearch = this.props.sales.filter(sale => sale.attributes.address.toLowerCase().includes(this.state.search))
+    .map(sale => (<ListGroup><ListGroupItem key={sale.id}><Link to={`/sales/${sale.id}`}>
+      {sale.attributes.address}</Link></ListGroupItem></ListGroup>))
+
+
+     return(
+       <>
+       <Card>
+         <Card.Img variant="top" src={banner1} />
+         <Card.Body>
+           <ListGroup className="list-group-flush">
+             <h1>Current Listings</h1>
+             <ListGroupItem>
+               {saleSearch}
+             </ListGroupItem>
+           </ListGroup>
+          </Card.Body>
+       </Card>
+       <form>
+         <input name="search" value={this.state.search} onChange={this.handleSearch}></input>
+       </form>
+       <button onClick={this.handleClick}>Livonia</button>
+
+       </>
+
+     )
+  }
 }
 
 const mapStateToProps = state => {
